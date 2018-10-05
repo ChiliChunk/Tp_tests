ac4ca1ad62ebf59a547e6445d5d58e2241f925e4
nicolas <nicolas.briet@outlook.com>
2018-10-05 15:20:27 +0200

    factorisation all test jauge

--- partie1/JaugeTest.java
+++ partie1/JaugeTest.java
@@ -35,22 +35,48 @@ public class JaugeTest {
 		return result;
 	}
 	
-	private JaugeNaturel j1 , j2 , j3 , j4 , j5 , j6 , j7 , j8 , j9 , j10;
+	private Jauge j1 , j2 , j3 , j4 , j5 , j6 , j7 , j8 , j9 , j10;
 	
 	@Before
 	public void setUp() throws Exception {
-		j1 = new JaugeNaturel(10, 100, 11); //verte
-		j2 = new JaugeNaturel(50, 100, 35); // bleu
-		j3 = new JaugeNaturel(0, 50, 60); // rouge
-		j4 = new JaugeNaturel (0,50,0); // departe == min
-		j5 = new JaugeNaturel(10, 50, 8); // depart < min
-		j6 = new JaugeNaturel(50, 40, 45); // max<depart<min
-		j7 = new JaugeNaturel(50, 50, 50); // max = min = depart
-		j8 = new JaugeNaturel(50, 50, 80); // max = min < depart
-		j9 = new JaugeNaturel(10, 40, 50); // min < max < depart
-		j10 = new JaugeNaturel(10, 50, 50); // min < max = depart
+//		j1 = new JaugeNaturel(10, 100, 11); //verte
+//		j2 = new JaugeNaturel(50, 100, 35); // bleu
+//		j3 = new JaugeNaturel(0, 50, 60); // rouge
+//		j4 = new JaugeNaturel (0,50,0); // departe == min
+//		j5 = new JaugeNaturel(10, 50, 8); // depart < min
+//		j6 = new JaugeNaturel(50, 40, 45); // max<depart<min
+//		j7 = new JaugeNaturel(50, 50, 50); // max = min = depart
+//		j8 = new JaugeNaturel(50, 50, 80); // max = min < depart
+//		j9 = new JaugeNaturel(10, 40, 50); // min < max < depart
+//		j10 = new JaugeNaturel(10, 50, 50); // min < max = depart
 	}
 	
+	@Test
+	public void runAllTests() {
+		for (type t : type.values()) {
+			j1 = creerJauge(t, 10, 100, 11); //verte
+			j2 = creerJauge(t, 50, 100, 35); // bleu
+			j3 = creerJauge(t, 0, 50, 60); // rouge
+			j4 = creerJauge(t, 0,50,0); // departe == min
+			j5 = creerJauge(t, 10, 50, 8); // depart < min
+			j6 = creerJauge(t, 50, 40, 45); // max<depart<min
+			j7 = creerJauge(t, 50, 50, 50); // max = min = depart
+			j8 = creerJauge(t, 50, 50, 80); // max = min < depart
+			j9 = creerJauge(t, 10, 40, 50); // min < max < depart
+			j10 = creerJauge(t, 10, 50, 50); // min < max = depart
+			
+			testJaugeNaturel();
+			testEstRouge();
+			testEstBleu();
+			testEstVert();
+			testDansIntervalle();
+			testDeplacement();
+			testInferieurInterval();
+			testLimiteVigieMaxInferieurVigieMin();
+			testMaxEgaleMin();
+			testSuperieurIntervalle();
+		}
+	}
 	@After
 	public void tearDown() throws Exception {
 		j1 = null;
@@ -58,27 +84,26 @@ public class JaugeTest {
 		j3 = null;
 	}
 
-	@Test
+	
 	public void testJaugeNaturel() {
 		assertNotNull("Ma jauge a bien ete creee" , j1);
 	}
 
-	@Test
 	public void testEstRouge() {
 		assertTrue("ma jauge est rouge" , j3.estRouge());
 	}
 
-	@Test
+	
 	public void testEstVert() {
 		assertTrue("ma jauge est verte" , j1.estVert());
 	}
 
-	@Test
+	
 	public void testEstBleu() {
 		assertTrue("ma jauge est verte" , j2.estBleu());
 	}
 	
-	@Test
+	
 	public void testDansIntervalle () {
 		assert j1.estVert() == true : "La jauge doit etre verte";
 		assert j1.estRouge() == false : "La jauge doit etre verte";
@@ -94,7 +119,7 @@ public class JaugeTest {
 //		
 	}
 	
-	@Test
+	
 	 public void testDeplacement ( ) {
 		j1.decrementer();
 		assert j1.estVert() == false : "La jauge doit etre bleu";
@@ -108,7 +133,7 @@ public class JaugeTest {
 	
 	}
 	
-	@Test
+	
 	public void testInferieurInterval() {
 		assert j4.estVert() == false : "La jauge doit etre bleu";
 		assert j4.estRouge() == false : "La jauge doit etre bleu";
@@ -119,7 +144,7 @@ public class JaugeTest {
 		assert j5.estBleu()== true : "La jauge doit etre bleu";
 	}
 	
-	@Test
+	
 	public void testLimiteVigieMaxInferieurVigieMin() {
 		assert j6.estBleu() == true : "La jauge doit etre bleue";
 		assert j6.estRouge() == true : "La jauge doit etre rouge";
@@ -127,7 +152,7 @@ public class JaugeTest {
 		// l'etat n'est pas coherent mais est autorisé dans ce programme, les tests peuvent donc passer
 	}
 	
-	@Test
+	
 	public void testMaxEgaleMin () {
 		assert j7.estBleu() == true : "La jauge doit etre bleue";
 		assert j7.estRouge() == true : "La jauge doit etre rouge";
@@ -140,7 +165,7 @@ public class JaugeTest {
 		//cet état est deja plus cohérent mais il faudrit que la jauge soit bleu quand val < min et pas val <= min, comme ça nous pourrions avoir une jauge verte quand max = min = depart
 	}
 	
-	@Test
+	
 	public void testSuperieurIntervalle() {
 		assert j9.estBleu() == false : "La jauge doit etre rouge";
 		assert j9.estRouge() == true : "La jauge doit etre rouge";
